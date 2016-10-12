4ffe031b3a705b2670b7d186d29499efc09f6852
Adam King <adam.king0126@gmail.com>
2016-10-12 11:21:07 -0500

    Done.

--- index.js
+++ index.js
@@ -5,14 +5,28 @@ var cake = {
   bakeTemp: "425 degrees",
   bakeTime: "45 minutes",
   customer: "Tommy",
-  decorate: function(updateFunction) {
+  decorate: function(updateFunction) { //using an arrow function here will break
+    //because the arrow function will scope 'this' to the window instead of the
+    //obj.
     var status = "Decorating with " + this.topping + ". Ready to eat soon!"
     updateFunction(status)
-    setTimeout(function() {
-      updateFunction(serve.apply(this, "Happy Eating!", this.customer))
+    setTimeout(() => {
+      //note how you must use brackets around the parameters you want to send
+      //in with 'apply'.  If we had used 'call', we could skip this, however
+      //spy is looking at apply so we're stuck with this bracket notation.
+      updateFunction(serve.apply(this, ["Happy Eating!", this.customer]))
     }, 2000)
   }
 }
+  // here's the code with the bind() function instead of arrow functions
+  // decorate: function(updateFunction) {
+  //   var status = "Decorating with " + this.topping + ". Ready to eat soon!"
+  //   updateFunction(status)
+  //   setTimeout(function() {
+  //     updateFunction(serve.apply(this, "Happy Eating!", this.customer))
+  //   }.bind(this), 2000)
+  // }
+
 
 var pie = {
   name: "Apple Pie",
@@ -23,32 +37,43 @@ var pie = {
   customer: "Tammy"
 }
 
+
 function makeCake() {
-  var updateCakeStatus;
-  mix(updateCakeStatus)
+  //need to make sure that the updateStatus grabs the correct DOM element
+  //called "status" - in this case, need to grab the cake status
+  var updateCakeStatus = updateStatus.bind(this);
+  updateCakeStatus("Prep"); //now call the bound function.
+  cake.decorate; //decorate is a function from inside the cake constructor
+  mix.call(cake, updateCakeStatus) //call the mix function (which does not
+  //belong to either cake or pie) and pass the updateFunction
 }
 
 function makePie() {
-  var updatePieStatus;
-  mix(updatePieStatus)
+  var updatePieStatus = updateStatus.bind(this);
+  updatePieStatus("Prep");
+  pie.decorate = cake.decorate.bind(pie); //borrow 'decorate' from cake obj.
+  mix.call(pie, updatePieStatus)
 }
 
 function updateStatus(statusText) {
   this.getElementsByClassName("status")[0].innerText = statusText
 }
 
-function bake(updateFunction) {
+function bake(updateFunction) { //updateFunction is the updateStatus function being called from the 'call' function calls (could be apply, too)
   var status = "Baking at " + this.bakeTemp + " for " + this.bakeTime
   setTimeout(function() {
-    cool(updateFunction)
-  }, 2000)
+    cool.call(this, updateFunction)
+    // cool(updateFunction)
+  }.bind(this), 2000) //because this is a nested function, we need to bind `this` from the parent.
+  updateFunction(status);
 }
 
 function mix(updateFunction) {
   var status = "Mixing " + this.ingredients.join(", ")
   setTimeout(function() {
-    bake(updateFunction)
-  }, 2000)
+    bake.call(this, updateFunction)
+    // bake(updateFunction)
+  }.bind(this), 2000)
   updateFunction(status)
 }
 
@@ -56,12 +81,16 @@ function cool(updateFunction) {
   var status = "It has to cool! Hands off!"
   setTimeout(function() {
     this.decorate(updateFunction)
-  }, 2000)
+  }.bind(this), 2000)
+  updateFunction(status);
 }
 
 function makeDessert() {
-  //add code here to decide which make... function to call
-  //based on which link was clicked
+  var node = this.parentNode;
+  //depending on which dessert the user wants to make, we need to scope the
+  //actions to the correct node - don't want to update cake status instead of
+  //pie status!
+  (node.id == "cake") ? makeCake.call(node) : makePie.call(node);
 }
 
 function serve(message, customer) {
