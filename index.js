09ef42b3305985ebeafe297631245d9b934c2b84
cassaram09 <cassaram09@gmail.com>
2017-03-16 19:19:54 -0400

    Done.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var cake = {
     }, 2000)
   }
 }
+
 var pie = {
   name: "Apple Pie",
   ingredients: ["apples", "flour", "eggs", "butter", "sugar"],
@@ -23,54 +24,59 @@ var pie = {
 }
 
 function makeCake() {
-  var updateCakeStatus = updateStatus
-  mix(updateCakeStatus)
+  var updateCakeStatus = updateStatus.bind(this)
+  updateCakeStatus("Prep")
+  mix.call(cake, updateCakeStatus)
 }
 
 function makePie() {
-  var updatePieStatus;
-  mix(updatePieStatus)
-  pie.decorate = cake.decorate.bind(pie);
+  var updatePieStatus = updateStatus.bind(this)
+  updatePieStatus("Prep")
+  pie.decorate = cake.decorate.bind(pie)
+  mix.call(pie, updatePieStatus)
 }
 
 function updateStatus(statusText) {
   this.getElementsByClassName("status")[0].innerText = statusText
 }
 
-function bake(updateFunction) {
-  var status = "Baking at " + this.bakeTemp + " for " + this.bakeTime
-  setTimeout(function() {
-    cool(updateFunction)
-  }, 2000)
-}
-
 function mix(updateFunction) {
   var status = "Mixing " + this.ingredients.join(", ")
-  setTimeout(function() {
-    bake(updateFunction)
+  setTimeout(() => {
+    bake.call(this, updateFunction)
+  }, 2000)
+  updateFunction(status)
+}
+
+function bake(updateFunction) {
+  var status = "Baking at " + this.bakeTemp + " for " + this.bakeTime
+  setTimeout(() => {
+    cool.call(this, updateFunction)
   }, 2000)
   updateFunction(status)
 }
 
 function cool(updateFunction) {
   var status = "It has to cool! Hands off!"
-  setTimeout(function() {
+  setTimeout(() => {
     this.decorate(updateFunction)
   }, 2000)
+  updateFunction(status)
 }
 
 function makeDessert() {
-  //add code here to decide which make... function to call
-  //based on which link was clicked
+  if(this.parentNode.id === "cake") {
+    makeCake.call(this.parentNode)
+  } else {
+    makePie.call(this.parentNode)
+  }
 }
 
 function serve(message, customer) {
-  //you shouldn't need to alter this function
   return(customer + ", your " + this.name + " is ready to eat! " + message)
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  //you shouldn't need to alter this function
   var cookLinks = document.getElementsByClassName("js-make")
   for(var i=0; i<cookLinks.length; i++) {
     cookLinks[i].addEventListener("click", makeDessert)
